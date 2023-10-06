08e505ff0c41858a703d3c1ca112c4ff40041b6c
manimegalai153 <147167102+manimegalai153@users.noreply.github.com>
2023-10-06 19:12:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+class player:
+    def play(selff):
+      print(" THE PLAYER IS PLAYIN CRICKET .")
+class Batsman(player):
+    def play(self):
+        print(" THE BATS MAN IS BATTING. ")
+class Bowler(player):
+    def play(self):
+        print(" THE BOWLER IS BOWLING. ")
+batsman = Batsman()
+bowler = Bowler()
+batsman.play()
+bowler.play()
